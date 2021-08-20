d4cf5b310c5a67050f92b31d22d47b671b834158
araviindm <69687740+araviindm@users.noreply.github.com>
2021-08-20 23:09:08 +0530

    Update StringCalculator.java
    handling new lines implemented

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,19 +1,18 @@
 public class StringCalculator{
+
     public static void main(String args[]) {
-        System.out.println(add("5,3"));
-      }
-     
-static int add(String numbers){
-    int total=0;
-    if(numbers.trim() != ""){
-        String[] num = numbers.split(",");
-        
-        for(String str: num){
-            total+= Integer.parseInt(str);
-        }
-        return total;
-    
+        System.out.println(add("5\n3,2"));
     }
-    return 0;
-     }
+    static int add(String numbers){
+
+        int total=0;
+        if(numbers.trim() != ""){
+            String[] num = numbers.split("[,\n]");
+            for(String str: num){
+                total+= Integer.parseInt(str);
+            }
+            return total;      
+         }
+        return 0;
+    }
   }
