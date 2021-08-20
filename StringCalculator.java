cd0ca9016173258ff654594e6065099d6670c114
araviindm <69687740+araviindm@users.noreply.github.com>
2021-08-20 23:04:28 +0530

    Update StringCalculator.java
    unknown amount of numbers implemented

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,17 +1,19 @@
 public class StringCalculator{
     public static void main(String args[]) {
-      System.out.println(add("1,2"));
-    }
-   
+        System.out.println(add("5,3"));
+      }
+     
 static int add(String numbers){
-       if(numbers.trim() != ""){
-           String[] num = numbers.split(",");
-           if(num.length == 1){
-               return Integer.parseInt(num[0]);
-           }else{
-               return Integer.parseInt(num[0])+Integer.parseInt(num[1]);
-           }
-       }
-       return 0;
-   }
-}
+    int total=0;
+    if(numbers.trim() != ""){
+        String[] num = numbers.split(",");
+        
+        for(String str: num){
+            total+= Integer.parseInt(str);
+        }
+        return total;
+    
+    }
+    return 0;
+     }
+  }
