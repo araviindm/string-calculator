87c0c3e3a2b06f0353ad7f004b1fc61531accfcd
araviindm <69687740+araviindm@users.noreply.github.com>
2021-08-20 23:34:04 +0530

    Update StringCalculator.java
    Exception added

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,21 +1,25 @@
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-  public static void main(String args[]) {
-    System.out.println(add("//;\n5;3"));
+  public static void main(String args[]) throws Exception {
+    System.out.println(add("1\n2,-3,-4"));
   }
 
-  static boolean isNumber(String num) {
+  static int getNumber(String num) {
+    int n = 0;
     try {
-      int n = Integer.parseInt(num);
+      n = Integer.parseInt(num);
     } catch (NumberFormatException err) {
-      return false;
+      return 0;
     }
-    return true;
+    return n;
   }
 
-  static int add(String numbers) {
+  static int add(String numbers) throws Exception {
     if (numbers.trim() != "") {
       String delimiter = "[,\n]+";
       if (Pattern.compile("^//").matcher(numbers).find()) {
@@ -24,10 +28,30 @@ public class StringCalculator {
       }
       String[] num = numbers.split(delimiter);
       int sum = 0;
+      boolean isNegativePresent = false;
       for (int i = 0; i < num.length; i++) {
-        if (isNumber(num[i])) {
-          sum += Integer.parseInt(num[i]);
+        int n = getNumber(num[i]);
+        if (n < 0) {
+          isNegativePresent = true;
+          break;
+        } else {
+          sum += n;
         }
+      }
+      if (isNegativePresent) {
+        List<Integer> arr = new ArrayList<Integer>();
+        for (int i = 0; i < num.length; i++) {
+          int n = getNumber(num[i]);
+          if (n < 0) {
+            arr.add(n);
+          }
+        }
+        String concat = arr
+          .stream()
+          .map(String::valueOf)
+          .collect(Collectors.joining(","));
+        String err = "\"negatives not allowed\" - " + concat;
+        throw new Exception(err);
       }
       return sum;
     }
